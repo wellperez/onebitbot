e392675ec34b0147d0e98213f3255d98a89c1f48
Wellington Perez <wellingtonaperez@gmail.com>
2017-05-20 11:23:04 -0300

    Services Created

--- app.rb
+++ app.rb
@@ -2,13 +2,29 @@ require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
 
-# Load Models
-Dir["./app/models/*.rb"].each {|file| require file }
-
 require './config/database'
+
+Dir["./app/models/*.rb"].each {|file| require file }
+Dir["./app/services/**/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
   get '/sinatra' do
     'Hello world Sinatra!'
+  end
+
+  post '/webhook' do
+    result = JSON.parse(request.body.read)["result"]
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+
+    content_type :json
+    {
+      "speech": response,
+      "displayText": response,
+      "source": "OneBitBot"
+    }.to_json
   end
 end
---
+++ app/services/faq/create_service.rb
@@ -0,0 +1,29 @@
+module FaqModule
+  class CreateService
+    def initialize(params)
+      # TODO: identify origin and set company
+      @company = Company.last
+      @question = params["question.original"]
+      @answer = params["answer.original"]
+      @hashtags = params["hashtags.original"]
+    end
+
+    def call
+      if @hashtags == nil || @hashtags == ""
+        return 'Hashtag Obrigatória'
+      end
+      
+      begin
+        Faq.transaction do
+          faq = Faq.create(question: @question, answer: @answer, company: @company)
+          @hashtags.split(/[\s,]+/).each do |hashtag|
+            faq.hashtags << Hashtag.create(name: hashtag)
+          end
+        end
+        "Criado com sucesso"
+      rescue
+        'Problemas na criação'
+      end
+    end
+  end
+end
