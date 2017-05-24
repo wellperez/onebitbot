caa0d5f81119984e4ff466425b900de292c7cf94
Wellington Perez <wellingtonaperez@gmail.com>
2017-05-24 11:01:14 -0300

    Refactored create, list and remove tests

--- spec/services/faq_link/list_service_spec.rb
+++ spec/services/faq_link/list_service_spec.rb
@@ -1,23 +1,23 @@
 require_relative './../../spec_helper.rb'
 
-describe FaqModule::CreateService do
+describe FaqLinkModule::CreateService do
   before do
     @company = create(:company)
   end
 
   describe '#call' do
     it "with list command: With zero faqs, return don't find message" do
-      @listService = FaqModule::ListService.new({}, 'list')
+      @listService = FaqLinkModule::ListService.new({}, 'list')
 
       response = @listService.call()
       expect(response).to match("Nada encontrado")
     end
 
     it "With two faqs, find questions and answer in response" do
-      @listService = FaqModule::ListService.new({}, 'list')
+      @listService = FaqLinkModule::ListService.new({}, 'list')
 
-      faq1 = create(:faq, company: @company)
-      faq2 = create(:faq, company: @company)
+      faq1 = create(:faq_link, company: @company)
+      faq2 = create(:faq_link, company: @company)
 
       response = @listService.call()
 
@@ -29,16 +29,16 @@ describe FaqModule::CreateService do
     end
 
     it "with search command: With empty query, return don't find message" do
-      @listService = FaqModule::ListService.new({'query' => ''}, 'search')
+      @listService = FaqLinkModule::ListService.new({'query' => ''}, 'search')
 
       response = @listService.call()
       expect(response).to match("Nada encontrado")
     end
 
     it "with search command: With valid query, find question and answer in response" do
-      faq = create(:faq, company: @company)
+      faq = create(:faq_link, company: @company)
 
-      @listService = FaqModule::ListService.new({'query' => faq.question.split(" ").sample}, 'search')
+      @listService = FaqLinkModule::ListService.new({'query' => faq.question.split(" ").sample}, 'search')
 
       response = @listService.call()
 
@@ -47,18 +47,18 @@ describe FaqModule::CreateService do
     end
 
     it "with search_by_hashtag command: With invalid hashtag, return don't find message" do
-      @listService = FaqModule::ListService.new({'query' => ''}, 'search_by_hashtag')
+      @listService = FaqLinkModule::ListService.new({'query' => ''}, 'search_by_hashtag')
 
       response = @listService.call()
       expect(response).to match("Nada encontrado")
     end
 
     it "with search_by_hashtag command: With valid hashtag, find question and answer in response" do
-      faq = create(:faq, company: @company)
+      faq = create(:faq_link, company: @company)
       hashtag = create(:hashtag, company: @company)
-      create(:faq_hashtag, faq: faq, hashtag: hashtag)
+      create(:faq_link_hashtag, faq_link: faq, hashtag: hashtag)
 
-      @listService = FaqModule::ListService.new({'query' => hashtag.name}, 'search_by_hashtag')
+      @listService = FaqLinkModule::ListService.new({'query' => hashtag.name}, 'search_by_hashtag')
 
       response = @listService.call()
 
--- spec/services/faq_link/remove_service_spec.rb
+++ spec/services/faq_link/remove_service_spec.rb
@@ -1,30 +1,30 @@
 require_relative './../../spec_helper.rb'
 
-describe FaqModule::RemoveService do
+describe FaqLinkModule::RemoveService do
   before do
     @company = create(:company)
   end
 
   describe '#call' do
     it "With valid ID, remove Faq" do
-      faq = create(:faq, company: @company)
-      @removeService = FaqModule::RemoveService.new({"id" => faq.id})
+      faq = create(:faq_link, company: @company)
+      @removeService = FaqLinkModule::RemoveService.new({"id" => faq.id})
       response = @removeService.call()
 
       expect(response).to match("Deletado com sucesso")
     end
 
     it "With valid ID, remove Faq from database" do
-      faq = create(:faq, company: @company)
-      @removeService = FaqModule::RemoveService.new({"id" => faq.id})
+      faq = create(:faq_link, company: @company)
+      @removeService = FaqLinkModule::RemoveService.new({"id" => faq.id})
 
-      expect(Faq.all.count).to eq(1)
+      expect(FaqLink.all.count).to eq(1)
       response = @removeService.call()
-      expect(Faq.all.count).to eq(0)
+      expect(FaqLink.all.count).to eq(0)
     end
 
     it "With invalid ID, receive error message" do
-      @removeService = FaqModule::RemoveService.new({"id" => rand(1..9999)})
+      @removeService = FaqLinkModule::RemoveService.new({"id" => rand(1..9999)})
       response = @removeService.call()
 
       expect(response).to match("Questão inválida, verifique o Id")
